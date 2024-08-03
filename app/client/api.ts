3baaf585118bacdd1fcf51f09aeafb44a44e0c22
Husnain <139365289+Niansuh@users.noreply.github.com>
2024-08-03 14:10:29 +0500

    Update api.ts

--- app/client/api.ts
+++ app/client/api.ts
@@ -145,7 +145,7 @@ export class ClientApi {
         {
           from: "human",
           value:
-            "Share from [NextChat]: https://github.com/Yidadaa/ChatGPT-Next-Web",
+            "Share from [GPT-ChatBot.ru]: https://gpt-chatbot.ru,
         },
       ]);
     // 敬告二开开发者们，为了开源大模型的发展，请不要修改上述消息，此消息用于后续数据清洗使用
